368391b1e497ff2efcacdb849fcdc5189d007ab6
harsh9975 <gautam33776@gmail.com>
2021-08-03 13:23:25 +0530

    Banner and Date Picker

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import './Banner.css';
 import {Button} from "@material-ui/core"
+// import Search from "../Search/Search.js"
+import DatePicker from "../DatePicker/DatePicker.js"
 
 function Banner(){
     const [showSearch, setShowSearch] = useState(false);
@@ -9,17 +11,17 @@ function Banner(){
     return (
         <div className="banner">
             <div className="banner-search">
-                {showSearch && <h1>Show Date Picker</h1>}
                 <Button onClick={()=> setShowSearch(!showSearch)} className="banner-search-button" variant="outlined">
-                    Search Dates
+                    {showSearch ? "Hide" : "Search Dates"}
                 </Button>
+                {showSearch && <DatePicker/>}
             </div>
             <div className="banner-info">
                 <h1>Get out stretch your imagination</h1>
                 <h5>Plan a diffrent kind of getaway to uncover the hidden gems near you</h5>
                 <Button variant="outlined">Explore Nearby</Button>
             </div>
-         
+            
         </div>
     )
 }
